5c4198e7b345f87e84406f2d47312c299c9344f8
Thanate boat Dhirasakdanon <dthanate@gmail.com>
2024-02-19 23:24:12 +0700

    ...

---
+++ {{ cookiecutter.repo_name }}/.ipython/profile_default/startup/50-custom.py
@@ -0,0 +1,5 @@
+from IPython.core.formatters import PlainTextFormatter
+from IPython.core.interactiveshell import InteractiveShell
+
+InteractiveShell.ast_node_interactivity = "all"
+PlainTextFormatter.max_seq_length = 50
