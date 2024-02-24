24662d60e031afb3f227c7011a4d70de9177db06
Thanate boat Dhirasakdanon <dthanate@gmail.com>
2024-02-24 18:56:59 +0700

    ...

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,6 +3,8 @@ mv .env_ .env
 mv .envrc_ .envrc
 mv .gitignore_ .gitignore
 pdm venv --python {{ cookiecutter.python_executable }} create
+mkdir -p .venv/etc/ptpython
+mv ptpython_config.py_ .venv/etc/ptpython/config.py
 pdm install -G all
 git init
 git add .
