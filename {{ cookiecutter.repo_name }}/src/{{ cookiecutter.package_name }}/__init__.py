5a52a85aafc05d8b6356cd952a33b1aed2204c55
Thanate boat Dhirasakdanon <dthanate@gmail.com>
2024-02-27 06:36:09 +0700

    ...

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/__init__.py
+++
@@ -1,5 +0,0 @@
-from typing import Final
-
-from .helpers.pyproject_helper import get_pyproject_standard_metadata
-
-__version__: Final[str] = str(get_pyproject_standard_metadata().version)
--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/helpers/path_helper.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/helpers/path_helper.py
@@ -1,15 +1,10 @@
-import os
+from os import path
 from pathlib import Path
 from typing import Final
 
 
-PROJECT_HOME: Final[str] = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../.."))
-PACKAGE_NAME: Final[str] = "{{ cookiecutter.package_name }}"
-
-
-def get_project_home() -> Path:
-    return Path(PROJECT_HOME)
+PACKAGE_ROOT: Final[str] = path.normpath(path.join(path.dirname(__file__), ".."))
 
 
 def get_package_root() -> Path:
-    return Path(PROJECT_HOME).joinpath("src").joinpath(PACKAGE_NAME)
+    return Path(PACKAGE_ROOT)
