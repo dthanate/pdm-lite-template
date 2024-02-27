f8f41c4bd88484699d98eb2614e2d609666cedb8
Thanate boat Dhirasakdanon <dthanate@gmail.com>
2024-02-27 12:09:26 +0700

    ...

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/helpers/json_helper.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/helpers/json_helper.py
@@ -1,49 +1,75 @@
+from io import IOBase
 from os import PathLike
 from pathlib import Path
 from typing import cast
 from typing import Optional
 from typing import TextIO
-from typing import Type
 from typing import TypeAlias
 
 import fsspec
+import pyjson5
 import ujson
-from fsspec.core import OpenFile
 from fsspec.spec import AbstractFileSystem
 
-JSONType: TypeAlias = dict[str, "JSONType"] | list["JSONType"] | int | str | float | bool | Type[None]
+JSONType: TypeAlias = dict[str, "JSONType"] | list["JSONType"] | int | str | float | bool | None
 
 
 def json_load(
-    flike: str | Path | PathLike[str] | TextIO | OpenFile,
+    flike: str | Path | PathLike[str] | TextIO,
+    /,
     *,
     compression: Optional[str] = None,
     filesystem: Optional[AbstractFileSystem] = None,
 ) -> JSONType:
-    if filesystem is not None:
-        with filesystem.open(
-            flike,
-            mode="rt",
-            encoding="utf-8",
-            compression=compression,
-        ) as f:
-            return cast(JSONType, ujson.load(cast(TextIO, f)))
     if isinstance(flike, (str, PathLike, Path)):
+        if filesystem is not None:
+            with filesystem.open(
+                flike,
+                mode="rt",
+                encoding="utf-8",
+                compression=compression,
+            ) as f:
+                return cast(JSONType, ujson.load(cast(IOBase, f)))
         with fsspec.open(
             flike,
             mode="rt",
             encoding="utf-8",
             compression=compression,
         ) as f:
-            return cast(JSONType, ujson.load(cast(TextIO, f)))
-    if isinstance(flike, OpenFile):
-        return cast(JSONType, ujson.load(cast(TextIO, flike)))
+            return cast(JSONType, ujson.load(cast(IOBase, f)))
     return cast(JSONType, ujson.load(flike))
+
+
+def json5_load(
+    flike: str | Path | PathLike[str] | TextIO,
+    /,
+    *,
+    compression: Optional[str] = None,
+    filesystem: Optional[AbstractFileSystem] = None,
+) -> JSONType:
+    if isinstance(flike, (str, PathLike, Path)):
+        if filesystem is not None:
+            with filesystem.open(
+                flike,
+                mode="rt",
+                encoding="utf-8",
+                compression=compression,
+            ) as f:
+                return cast(JSONType, pyjson5.decode_io(cast(IOBase, f)))
+        with fsspec.open(
+            flike,
+            mode="rt",
+            encoding="utf-8",
+            compression=compression,
+        ) as f:
+            return cast(JSONType, pyjson5.decode_io(cast(IOBase, f)))
+    return cast(JSONType, pyjson5.decode_io(cast(IOBase, flike)))
 
 
 def json_dump(
     obj: JSONType,
-    flike: str | Path | PathLike[str] | TextIO | OpenFile,
+    flike: str | Path | PathLike[str] | TextIO,
+    /,
     *,
     indent: int = 0,
     ensure_ascii: bool = False,
@@ -58,15 +84,12 @@ def json_dump(
             ensure_ascii=ensure_ascii,
         )
 
-    if filesystem is not None:
-        with filesystem.open(flike, mode="wt", encoding="utf-8", compression=compression) as f:
-            do_dump(cast(TextIO, f))
-            return
     if isinstance(flike, (str, PathLike, Path)):
+        if filesystem is not None:
+            with filesystem.open(flike, mode="wt", encoding="utf-8", compression=compression) as f:
+                do_dump(cast(TextIO, f))
+                return
         with fsspec.open(flike, mode="wt", encoding="utf-8", compression=compression) as f:
             do_dump(cast(TextIO, f))
             return
-    if isinstance(flike, OpenFile):
-        do_dump(cast(TextIO, flike))
-        return
     do_dump(flike)
